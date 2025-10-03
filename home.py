dd2638288a6cfec8f45c3a43b7c72198a7fe3aaf
DrEK <162624985+ekonkati@users.noreply.github.com>
2025-10-04 00:36:05 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-st.set_page_config(page_title="Pilecap Suite", layout="wide")
-st.title("Pilecap Design Suite")
+st.set_page_config(page_title="Lfill Suite", layout="wide")
+st.title("SLF Design Suite")
 
 st.markdown("""
 Welcome! Use the **Pages** menu (left sidebar) to open:
-- **1 Pile, 2 Piles..etc.
+- **SLF Design & Costing.
 
 """)
+
